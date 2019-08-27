3f128aaf9754670bfbe26102a61590cbcf1224f7
Daniel Jorge <djpgonzaga@hotmail.com>
2019-08-27 20:20:35 -0300

    grid column test

---
+++ src/app/views/grid-test/grid-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-test',
+  templateUrl: './grid-test.component.html',
+  styleUrls: ['./grid-test.component.scss']
+})
+export class GridTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
