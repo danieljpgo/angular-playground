1e5b4106ad57149d7b6d5f9744386aa3207a08b7
Daniel Jorge <djpgonzaga@gmail.com>
2019-08-27 19:38:30 -0300

    disable ivy and test hammer js with mat-tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  selectedIndex: number = 0;
+  tabs: number = 3;
+
   title = 'angular-playground';
+
+  onSwipeleft(event) {
+    console.log('teste2');
+    if(this.selectedIndex + 1 <= this.tabs - 1) {
+      this.selectedIndex += 1;
+    }
+  }
+
+  onSwiperight(event) {
+    console.log('teste');
+    if(this.selectedIndex - 1 >= 0) {
+      this.selectedIndex -= 1;
+    }
+  }
 }
